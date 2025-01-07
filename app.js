b4aa157df10833755f70d484f0803bd5706a62ba
GhaaZaaal <aghazal88@gmail.com>
2025-01-07 05:58:19 +0200

    Added Controllers Dir & Categories Controllers - CRUD Operations

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv/config');
+
+const dbConnection = require('./config/db');
+
+dbConnection();
+
+const categoryApi = require('./apis/categoryApi');
+const ApiError = require('./utils/apiError');
+const globalError = require('./middlewares/errorMiddleware');
+
+// Express App
+const app = express();
+
+// Middleware for parsing JSON (in case you want to accept JSON requests)
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'Development') {
+  app.use(morgan('dev'));
+  console.log(`Mode: ${process.env.NODE_ENV}`);
+}
+
+const apiV = process.env.API_V;
+app.use(`${apiV}/categories`, categoryApi);
+// Create Error and Send it To Error Handling Middleware
+app.all('*', (req, res, next) => {
+  next(new ApiError(`Can't Find This Route: ${req.originalUrl}`, 400));
+});
+
+// Global Error Handling Middleware
+app.use(globalError);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
---
+++ controllers/categoryControllers.js
@@ -0,0 +1,91 @@
+const Category = require('../models/categoryModel');
+const asyncHandler = require('express-async-handler');
+const globalError = require('../middlewares/errorMiddleware');
+const ApiError = require('../utils/apiError');
+const slugify = require('slugify');
+
+// @desc:   Create A New Category
+// @route:  POST {API_V}/categories
+// @access: Private
+exports.createCategories = asyncHandler(async (req, res, next) => {
+  const name = req.body.name;
+  console.log(name);
+
+  const category = await Category.create({ name, slug: slugify(name) });
+  res.status(201).json({ data: category });
+});
+
+// @desc:   Get A Specific Category
+// @route:  GET {API_V}/categories/:id
+// @access: Public
+exports.getCategory = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const category = await Category.findById(id);
+  if (!category) {
+    return next(new ApiError(`No Category For This Id ${id}`, 404));
+  }
+  console.log(category.name);
+
+  res.status(200).json({ data: category });
+});
+
+// @desc:   Update A Specific Category
+// @route:  PUT {API_V}/categories/:id
+// @access: Private
+exports.updateCategory = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const { name } = req.body;
+
+  const category = await Category.findOneAndUpdate({ _id: id }, { name, slug: slugify(name) }, { new: true });
+  if (!category) {
+    return next(new ApiError(`No Category For This Id ${id}`, 404));
+  }
+  console.log(category.name);
+
+  res.status(200).json({ data: category });
+});
+
+// @desc:   Delete A Specific Category
+// @route:  DELETE {API_V}/categories/:id
+// @access: Private
+exports.deleteCategory = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const category = await Category.findByIdAndDelete(id);
+  if (!category) {
+    return next(new ApiError(`No Category For This Id ${id}`, 404));
+  }
+  console.log(category.name);
+
+  res.status(200).send();
+});
+
+// @desc:   Get A List Of Category
+// @route:  GET {API_V}/categories
+// @access: Public
+exports.getCategories = asyncHandler(async (req, res, next) => {
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 5;
+  const skip = (page - 1) * limit; // (3 - 1) * 5 = 5
+  const categories = await Category.find({}).skip(skip).limit(limit);
+  res.status(200).json({ results: categories.length, page, data: categories });
+});
+
+// method 3 and final
+// async await
+// method 2
+// Category.create({ name, slug: slugify(name) })
+//   .then((category) => res.status(201).json({ data: category }))
+//   .catch((err) => res.status(400).send(err));
+// method 1
+// const newCategory = new Category({ name });
+// newCategory
+//   .save()
+//   .then((doc) => {
+//     res.json(doc);
+//   })
+//   .catch((err) => {
+//     res.json(err);
+//   });
