2f78c1cf934ef9ea3e3c4ede15ac936131e0e5fb
GhaaZaaal <aghazal88@gmail.com>
2025-01-21 18:59:32 +0200

    Updated The Error Handling Middleware To Handle Token Errors (If Updated)

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,4 +1,5 @@
 require('dotenv/config');
+const ApiError = require('../utils/apiError');
 
 const sendErrorForDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -16,13 +17,22 @@ const sendErrorForProduction = (err, res) => {
   });
 };
 
+const handleJwtInvalidSignatureError = () =>
+  new ApiError('Invalid Token, Please Login Again', 401);
+
+const handleJwtExpiredError = () =>
+  new ApiError('Token Expired, Please Login Again', 401);
+
 const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  
+
   if (process.env.NODE_ENV === 'Development') {
     sendErrorForDev(err, res);
   } else if (process.env.NODE_ENV === 'Production') {
+    if (err.name === 'JsonWebTokenError')
+      err = handleJwtInvalidSignatureError();
+    if (err.name === 'TokenExpiredError') err = handleJwtExpiredError();
     sendErrorForProduction(err, res);
   }
 };
