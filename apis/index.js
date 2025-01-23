4727f8856bcd5d37e84cd1c4b32b4e3680ec4ba5
GhaaZaaal <aghazal88@gmail.com>
2025-01-23 16:28:43 +0200

    Refactoring Mounted APIs (Created a Separate File To Mount APIs)

---
+++ apis/index.js
@@ -0,0 +1,25 @@
+require('dotenv/config');
+
+const categoryApi = require('./categoryApi');
+const subCategoryApi = require('./subCategoryApi');
+const brandApi = require('./brandApi');
+const productApi = require('./productApi');
+const userApi = require('./userApi');
+const authApi = require('./authApi');
+const reviewApi = require('./reviewApi');
+const wishListApi = require('./wishListApi');
+
+const apiV = process.env.API_V;
+
+const mountApis = (app) => {
+  app.use(`${apiV}/categories`, categoryApi);
+  app.use(`${apiV}/subCategories`, subCategoryApi);
+  app.use(`${apiV}/brands`, brandApi);
+  app.use(`${apiV}/products`, productApi);
+  app.use(`${apiV}/users`, userApi);
+  app.use(`${apiV}/auth`, authApi);
+  app.use(`${apiV}/reviews`, reviewApi);
+  app.use(`${apiV}/wishList`, wishListApi);
+};
+
+module.exports = mountApis;
